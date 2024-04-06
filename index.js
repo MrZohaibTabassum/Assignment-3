dc1b63cdf27a3cfd47cbbf28a9ff7cc49b479487
Muhammad Zohaib Tabassum <zohaibtabassum76@gmail.com>
2024-04-07 00:05:10 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+// Declare a variable for storing your favorite movie name with a string type annotation.
+let fovourite = "the avengeres";
+console.log(fovourite);
+// Create a template string to introduce yourself, including your name and age.
+let myName = "M.Zohaib Tabassum";
+let age = 22;
+let introduce = `Hello, my name is ${myName} and i am ${age} year old`;
+console.log(introduce);
+// Initialize a constant with a type annotation for your current mood as a boolean.
+const current_mood = true;
+console.log(current_mood);
+// Combine a user's first and last name using template literals and log the full name.
+let first_name = "M.Zohaib";
+let last_name = "Tabassum";
+let full_Name = `${first_name}${last_name}`;
+console.log(full_Name);
+// Create a variable for a user's age with a type annotation and assign a number to it.
+let myAge = 22;
+console.log(myAge);
+// Use a template string to display a product name and its price.
+let product_name = "Laptop";
+let price = 68000;
+let bill = `product name: ${product_name}, Price= ${price}`;
+console.log(bill);
+// Declare a variable to store the number of pets you have with a number type annotation.
+let countpet;
+// Declare a variable with a boolean type annotation indicating if it is your birthday today.
+let birthday_today = false;
+// Create a template string that includes a user's first name, last name, and age with proper labels.
+let information = `First_Name: ${first_name}, Last_Name: ${last_name}, Age ${age}`;
+console.log(information);
+// Initialize a constant to hold your birth year with a number type annotation.
+const birth_year = 2001;
+// Write a line of code that uses a template string to combine a city and country into a full address.
+let city = "lahore";
+let country = "Pakistan";
+let address = `city: ${city} , country ${country}: `;
+console.log(address);
+// Declare a variable for today's temperature with a number type annotation and assign a value to it.
+let temperature_today = 21;
+console.log(temperature_today);
